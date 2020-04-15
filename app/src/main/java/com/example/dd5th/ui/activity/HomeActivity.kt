e47f83aff6ae5cd4ba0c9b59c0eee9c9385410fb
Bernardo Junior <bernardodemsj@gmail.com>
2020-04-15 14:13:11 -0300

    set layout to homeActivity

--- app/src/main/java/com/example/dd5th/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/dd5th/ui/activity/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.example.dd5th.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dd5th.R
 
-class HomeActivity: AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
     }
 
 }
