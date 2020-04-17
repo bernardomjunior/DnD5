807775d85407590711aaf350fd4828769abd6c9b
Bernardo Junior <bernardodemsj@gmail.com>
2020-04-17 16:41:48 -0300

    home activity with onclick starting another activity

--- app/src/main/java/com/example/dd5th/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/dd5th/ui/activity/HomeActivity.kt
@@ -1,57 +1,61 @@
 package com.example.dd5th.ui.activity
 
-
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dd5th.R
-import com.example.dd5th.data.external.ApiOptionsRepository
+import com.example.dd5th.data.external.ApiRepository
 import com.example.dd5th.ui.adapter.HomeListAdapter
-import kotlinx.android.synthetic.main.generic_listing.*
+import kotlinx.android.synthetic.main.generic_listing.list_app_options
+
 class HomeActivity : AppCompatActivity() {
 
     private val options = HashMap<String, String>()
-    private val repository = ApiOptionsRepository(this)
+    private val repository = ApiRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.generic_listing)
         setList()
-        repository.listApiOptions()
+        repository.listApiOptions(this)
     }
 
     private fun setList() {
         list_app_options.layoutManager = LinearLayoutManager(this)
-        list_app_options.adapter = HomeListAdapter(options, this) {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        }
+        list_app_options.adapter =
+            HomeListAdapter(options, this) { option: String, optionValue: String ->
+                val intent = Intent(applicationContext, ResourceListActivity::class.java)
+                intent.putExtra("option", option)
+                intent.putExtra("optionValue", optionValue)
+                startActivity(intent)
+            }
     }
 
-    fun upDateOptionList(dictionary: HashMap<String, String>){
+    fun upDateOptionList(dictionary: HashMap<String, String>) {
         options.clear()
         options.putAll(updateOptionsKeys(dictionary))
         list_app_options.adapter?.notifyDataSetChanged()
     }
 
 
-    fun updateOptionsKeys(dictionary: HashMap<String, String>): HashMap<String, String>{
+    fun updateOptionsKeys(dictionary: HashMap<String, String>): HashMap<String, String> {
 
         val newDictionary = HashMap<String, String>()
         val dictionaryKeys = dictionary.keys
-        dictionaryKeys.forEach{ key ->
+        dictionaryKeys.forEach { key ->
             println(key)
             dictionary[key]?.let {
-                newDictionary[capitalizeOption(key)] =  dictionary[key]!!
+                newDictionary[capitalizeOption(key)] = dictionary[key]!!
             }
         }
         return newDictionary
     }
 
-    fun capitalizeOption(option: String): String{
+    fun capitalizeOption(option: String): String {
         var newStr = ""
         option.split("-").forEach {
-            newStr+= it[0].toUpperCase() + it.slice(1 until it.length) + " "
+            newStr += it[0].toUpperCase() + it.slice(1 until it.length) + " "
         }
         return newStr
     }
--- app/src/main/java/com/example/dd5th/ui/adapter/HomeListAdapter.kt
+++ app/src/main/java/com/example/dd5th/ui/adapter/HomeListAdapter.kt
@@ -6,25 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dd5th.R
-import kotlinx.android.synthetic.main.home_options_list_item.view.textview_item_options
+import kotlinx.android.synthetic.main.list_item.view.textview_item_options
 
 class HomeListAdapter(
     private val options: HashMap<String, String>,
     private val context: Context,
-    private val listener: (String) -> Unit
+    private val listener: (String, String) -> Unit
 ) : RecyclerView.Adapter<HomeListAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(option: String, listener: (String) -> Unit) {
+        fun bind(option: String, optionValue: String, listener: (String, String) -> Unit) {
             itemView.textview_item_options.text = option
-            itemView.setOnClickListener { listener(option) }
+            itemView.setOnClickListener { listener(option, optionValue) }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(context).inflate(
-                R.layout.home_options_list_item,
+                R.layout.list_item,
                 parent,
                 false
             )
@@ -36,6 +36,7 @@ class HomeListAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.bind(options.keys.toList().sorted()[position], listener)
+        val key = options.keys.toList().sorted()[position]
+        holder.bind(key, options[key]!!, listener)
     }
 }
