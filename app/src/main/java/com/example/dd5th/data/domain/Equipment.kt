0effd835ac5868c3ab21a3fc3e1c7a2b455fc24e
Bernardo Junior <bernardodemsj@gmail.com>
2020-04-22 14:42:34 -0300

    update equipment activity

--- app/src/main/java/com/example/dd5th/data/domain/Equipment.kt
+++ app/src/main/java/com/example/dd5th/data/domain/Equipment.kt
@@ -1,13 +1,16 @@
 package com.example.dd5th.data.domain
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Equipment (
     val index: String,
     val name: String,
+    @SerializedName("equipment_category")
     val equipmentCategory: String,
+    @SerializedName("gear_category")
     val gearCategory: String,
     val cost: Cost,
     val weight: Int
--- app/src/main/java/com/example/dd5th/ui/activity/EquipmentActivity.kt
+++ app/src/main/java/com/example/dd5th/ui/activity/EquipmentActivity.kt
@@ -3,11 +3,42 @@ package com.example.dd5th.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dd5th.R
+import com.example.dd5th.data.domain.Equipment
+import com.example.dd5th.data.external.ApiRepository
+import kotlinx.android.synthetic.main.activity_equipment.*
 
 class EquipmentActivity: AppCompatActivity() {
+
+    private lateinit var equipment: Equipment
+    private val api = ApiRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equipment)
+        val equipmentIndex = getEquipmentIndexExtra()
+        equipmentIndex?.let {
+            api.getEquipment(it, this)
+        }
     }
+
+    private fun fillFields() {
+        txt_equipment_name.text = equipment.name
+        txt_equipment_category.text = equipment.equipmentCategory
+        txt_equipment_gear_category.text = equipment.gearCategory
+    }
+
+    private fun getEquipmentIndexExtra() : String?{
+        intent.extras?.let {
+            if (it.containsKey("equipment")) {
+                return it.getString("equipment")
+            }
+        }
+        return null
+    }
+
+    fun onEquipmentResult(equipment: Equipment){
+        this.equipment = equipment
+        fillFields()
+    }
+
 }
