b74efbb384c608a07bae4f33c5e7a1b5e3c2566e
Bernardo Junior <bernardodemsj@gmail.com>
2020-04-21 22:57:40 -0300

    creation of equipment activity

---
+++ app/src/main/java/com/example/dd5th/ui/activity/EquipmentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dd5th.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dd5th.R
+
+class EquipmentActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_equipment)
+    }
+}
