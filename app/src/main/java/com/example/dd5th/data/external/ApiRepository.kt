b2f395901404f921574806869fad0a5a6d71002f
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-11 15:58:18 -0300

    Turn Api into simple class

--- app/src/main/java/com/example/dd5th/data/external/ApiRepository.kt
+++ app/src/main/java/com/example/dd5th/data/external/ApiRepository.kt
@@ -17,7 +17,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object ApiRepository : GenericResourceListingContract.Api,
+class ApiRepository : GenericResourceListingContract.Api,
     SpecificResourceListingContract.Api,
     EquipmentContract.Api,
     LanguageContract.Api{
