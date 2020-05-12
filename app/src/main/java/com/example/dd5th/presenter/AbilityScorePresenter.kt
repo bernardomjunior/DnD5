c093d77f4e3293241849d47b34319a70ac15a8ac
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-12 13:43:31 -0300

    Add Ability Score presenter

---
+++ app/src/main/java/com/example/dd5th/presenter/AbilityScorePresenter.kt
@@ -0,0 +1,33 @@
+package com.example.dd5th.presenter
+
+import android.os.Bundle
+import com.example.dd5th.contract.AbilityScoreContract
+import com.example.dd5th.data.domain.AbilityScore
+import com.example.dd5th.data.external.ApiRepository
+
+class AbilityScorePresenter(
+    private val view: AbilityScoreContract.View
+): AbilityScoreContract.Presenter, AbilityScoreContract.Callback {
+
+    private val api: AbilityScoreContract.Api = ApiRepository()
+
+    override fun getAbilityScore(extras: Bundle?) {
+        extras?.let { e ->
+            e.getString("ability-scores")?.let {
+                api.getAbilityScore(it, this)
+            }
+        }
+    }
+
+    override fun onSuccess(abilityScore: AbilityScore) {
+        view.showAbilityScore(abilityScore)
+    }
+
+    override fun onError() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFailure() {
+        TODO("Not yet implemented")
+    }
+}
