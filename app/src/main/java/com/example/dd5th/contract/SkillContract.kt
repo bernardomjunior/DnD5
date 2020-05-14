2e3e71833721ffa5b0efdc9e675b7be461f761ab
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-14 11:25:50 -0300

    Adds skill contract

---
+++ app/src/main/java/com/example/dd5th/contract/SkillContract.kt
@@ -0,0 +1,26 @@
+package com.example.dd5th.contract
+
+import android.os.Bundle
+import com.example.dd5th.data.domain.Skill
+
+interface SkillContract {
+
+    interface View {
+        fun onSkillResult(skill: Skill)
+    }
+
+    interface Presenter{
+        fun getSkillIfExtra(extras: Bundle?)
+    }
+
+    interface Callback{
+        fun onSuccess(skill: Skill)
+        fun onFailure()
+        fun onError()
+    }
+
+    interface Api{
+        fun getSkill(skillName: String)
+    }
+
+}
