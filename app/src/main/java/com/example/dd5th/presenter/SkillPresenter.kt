366a8fa166147e8b8295b59c11a3ccc8bd3084fe
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-14 14:05:40 -0300

    Adds skill presenter

---
+++ app/src/main/java/com/example/dd5th/presenter/SkillPresenter.kt
@@ -0,0 +1,33 @@
+package com.example.dd5th.presenter
+
+import android.os.Bundle
+import com.example.dd5th.contract.SkillContract
+import com.example.dd5th.data.domain.Skill
+import com.example.dd5th.data.external.ApiRepository
+
+class SkillPresenter(
+    private val view: SkillContract.View
+): SkillContract.Presenter, SkillContract.Callback {
+
+    private val api: SkillContract.Api = ApiRepository
+
+    override fun getSkillIfExtra(extras: Bundle?) {
+        extras?.let { e ->
+            e.getString(SKILLS)?.let {
+                api.getSkill(it, this)
+            }
+        }
+    }
+
+    override fun onSuccess(skill: Skill) {
+        view.onSkillResult(skill)
+    }
+
+    override fun onFailure() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onError() {
+        TODO("Not yet implemented")
+    }
+}
