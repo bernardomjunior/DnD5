be27efca34c9440dad8cabd5ff3f0d6e5d5fe8f6
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-14 11:13:36 -0300

    Adds skill activity

---
+++ app/src/main/java/com/example/dd5th/ui/activity/SkillActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dd5th.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dd5th.R
+
+class SkillActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+
+}
